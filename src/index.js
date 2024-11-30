7f54056d2dd3a29f22825b05a8dbbaec2a5e3139
A-Harshavardhan <ananthamharsha333@gmail.com>
2024-11-30 13:57:52 +0530

    get method in express.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,25 @@ const express = require('express');
 // creates a new Express Application and assigns to app variable. Which we can then use to define routes, middlewares and set up server.
 const app = express();
 
-app.listen(3000, () => console.log("Port started at 3000"));
+app.get('/products', (req, res) => {
+
+    // Is a response method used to send response back to client
+    // res.send([
+    //     {
+    //         item : 'iPhone-15 pro max',
+    //         quantity : '2'
+    //     },
+    //     {
+    //         item : 'Samsung Galaxy',
+    //         quantity : '1'
+    //     }
+    // ]);
+
+    // Is a response method used to sendStatus to the client
+    // res.sendStatus(500);
+
+    // if we want to sent the both status and message we can do in the below way.
+    res.status(500).json({ message : 'Something went wrong!' });
+});
+
+app.listen(3000, () => console.log("Port started successfully at 3000"));
